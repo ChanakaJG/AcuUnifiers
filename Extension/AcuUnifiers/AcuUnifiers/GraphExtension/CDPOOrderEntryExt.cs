c9f32821fce8ba66758a277552109c574a60fa55
Saranga Vitharana <Saranga.Vitharana@acumatica.com>
2024-10-24 17:56:08 +0530

    Added more validations to POOrderEntry on vendor and vendor location changed.

--- Extension/AcuUnifiers/AcuUnifiers/GraphExtension/CDPOOrderEntryExt.cs
+++ Extension/AcuUnifiers/AcuUnifiers/GraphExtension/CDPOOrderEntryExt.cs
@@ -19,5 +19,210 @@ namespace AcuUnifiers
             if (!MergeVendorScope.IsActive)
                 baseInvoke?.Invoke(sender, order, newVendorID, error);
         }
+
+        protected void _(Events.FieldDefaulting<POOrder.termsID> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.TermsID;
+            }
+        }
+        protected void _(Events.FieldDefaulting<POOrder.ownerID> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.OwnerID;
+            }
+        }
+        protected void _(Events.FieldDefaulting<POOrder.orderDate> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.OrderDate;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.branchID> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.BranchID;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.taxCalcMode> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.TaxCalcMode;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.shipVia> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.ShipVia;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.fOBPoint> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.FOBPoint;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.expectedDate> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.ExpectedDate;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.siteID> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.SiteID;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.approved> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.Approved;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.dontPrint> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.DontPrint;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.dontEmail> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.DontEmail;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.emailed> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.Emailed;
+            }
+        }
+        protected void _(Events.FieldDefaulting<POOrder.printed> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.Printed;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.shipDestType> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.ShipDestType;
+            }
+        }
+
+        protected void _(Events.FieldDefaulting<POOrder.shipToLocationID> e, PXFieldDefaulting del)
+        {
+            if (!MergeVendorScope.IsActive)
+            {
+                del?.Invoke(e.Cache, e.Args);
+            }
+            else
+            {
+                POOrder order = e.Row as POOrder;
+                e.NewValue = order.ShipToLocationID;
+            }
+        }
     }
 }
