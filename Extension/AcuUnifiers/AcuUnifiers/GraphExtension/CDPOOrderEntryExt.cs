a652fa7d1eed47fe4bf75b9ddeb78811327ce0c5
Saranga Vitharana <Saranga.Vitharana@acumatica.com>
2024-10-24 17:35:34 +0530

    Added POOrderEntry and added validation logics.

---
+++ Extension/AcuUnifiers/AcuUnifiers/GraphExtension/CDPOOrderEntryExt.cs
@@ -0,0 +1,23 @@
+﻿using PX.Data;
+using PX.Objects.PO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcuUnifiers
+{
+    public class CDPOOrderEntryExt : PXGraphExtension<POOrderEntry>
+    {
+        public static bool IsActive() => true;
+
+        public delegate void RaiseVendorIDExceptionDel(PXCache sender, POOrder order, object newVendorID, string error);
+        [PXOverride]
+        public virtual void RaiseVendorIDException(PXCache sender, POOrder order, object newVendorID, string error, RaiseVendorIDExceptionDel baseInvoke)
+        {
+            if (!MergeVendorScope.IsActive)
+                baseInvoke?.Invoke(sender, order, newVendorID, error);
+        }
+    }
+}
